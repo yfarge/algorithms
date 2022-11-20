594caf21b2283dfce03751cbdb791e75cfa92db9
Yousef Farge <yousef.farge@gmail.com>
2022-11-19 18:25:37 -0600

    added ListNode class to rotate-list

--- rotate-list/index.py
+++ rotate-list/index.py
@@ -2,10 +2,12 @@ from typing import *
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head:
