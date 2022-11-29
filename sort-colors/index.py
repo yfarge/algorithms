99639276be9dbf2909b209030e1cbb1f6543b777
Yousef Farge <yousef.farge@gmail.com>
2022-11-29 11:22:18 -0600

    updated sort-colors to use Dijkstra's three pointer approach

--- sort-colors/index.py
+++ sort-colors/index.py
@@ -1,16 +1,25 @@
 from typing import *
 
 
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        counter = Counter(nums)
+def sortColors(nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    l = 0
+    r = len(nums) - 1
+    i = 0
+    while i < len(nums) and l <= r and i <= r:
+        if nums[i] == 0:
+            nums[l], nums[i] = nums[i], nums[l]
+            l += 1
+            i += 1
+        elif nums[i] == 2:
+            nums[r], nums[i] = nums[i], nums[r]
+            r -= 1
+        else:
+            i += 1
 
-        i = 0
-        for j in range(3):
-            while counter[j]:
-                nums[i] = j
-                counter[j] -= 1
-                i += 1
+
+nums = [2, 0, 2, 1, 1, 0]
+sortColors(nums)
+print(nums)
