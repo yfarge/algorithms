98a870f3cf910cda82b7f6df6542a3be14b4cbb8
Yousef Farge <yousef.farge@gmail.com>
2022-11-29 00:36:32 -0600

    wrapped sort-colors in Solution class

--- sort-colors/index.py
+++ sort-colors/index.py
@@ -1,18 +1,16 @@
 from typing import *
 
 
-def sortColors(nums: List[int]) -> None:
-    counter = Counter(nums)
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        counter = Counter(nums)
 
-    i = 0
-    for j in range(3):
-        while counter[j]:
-            nums[i] = j
-            counter[j] -= 1
-            i += 1
-
-
-nums = [2, 1]
-
-sortColors(nums)
-print(nums)
+        i = 0
+        for j in range(3):
+            while counter[j]:
+                nums[i] = j
+                counter[j] -= 1
+                i += 1
