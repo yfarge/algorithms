a30d1586c3ea1601f173a7455cd4a6f1bda00f82
Yousef Farge <yousef.farge@gmail.com>
2022-11-29 19:04:35 -0600

    renamed dict d to seen in two-sum

--- two-sum/index.py
+++ two-sum/index.py
@@ -3,10 +3,10 @@ from typing import *
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
+        seen = {}
         for i, num in enumerate(nums):
-            j = d.get(target - num)
+            j = seen.get(target - num)
             if j != None:
                 return [j, i]
-            d[num] = i
+            seen[num] = i
         return []
