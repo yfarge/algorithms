6988bf0d83e360b73c31937deacccf04f0398b5c
Yousef Farge <yousef.farge@gmail.com>
2023-09-11 00:31:43 -0500

    merge sorted array

---
+++ merge-sorted-array/index.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i, j = m - 1, n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[i + j + 1] = nums1[i]
+                i -= 1
+            else:
+                nums1[i + j + 1] = nums2[j]
+                j -= 1
+
+        while j >= 0:
+            nums1[i + j + 1] = nums2[j]
+            j -= 1
