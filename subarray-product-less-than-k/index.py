e32a12032107c661813e1c63a7383138c9c13c01
Yousef Farge <yousef.farge@gmail.com>
2023-09-14 01:24:13 -0500

    subarray product less than k

---
+++ subarray-product-less-than-k/index.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        count = 0
+        left = 0
+        prod = 1
+        for right in range(len(nums)):
+            prod *= nums[right]
+            while left <= right and prod >= k:
+                prod //= nums[left]
+                left += 1
+            count += right - left + 1
+
+        return count
