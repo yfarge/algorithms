004ef910cd075b25c261db2a69c30cb62eeacf01
Yousef Farge <yousef.farge@gmail.com>
2024-07-14 11:32:40 -0500

    delete-node-in-a-bst

---
+++ delete-node-in-a-bst/index.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+            dummy = root.right
+            while dummy:
+                root.val = dummy.val
+                dummy = dummy.left
+
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
