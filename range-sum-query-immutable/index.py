2afaf901c0070f9e2a20062cc0df4b8006d95d80
Yousef Farge <yousef.farge@gmail.com>
2024-07-24 19:45:17 -0500

    range-sum-query-immutable

---
+++ range-sum-query-immutable/index.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = [0 for _ in range(len(nums) + 1)]
+        for i in range(len(nums)):
+            self.prefix[i + 1] = self.prefix[i] + nums[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
