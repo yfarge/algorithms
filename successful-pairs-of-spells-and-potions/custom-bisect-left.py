ea653eff43b0f3238dd59b1897067331b8da9a46
Yousef Farge <yousef.farge@gmail.com>
2024-07-25 19:48:26 -0500

    successful-pairs-of-spells-and-potions

---
+++ successful-pairs-of-spells-and-potions/custom-bisect-left.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def bisectLeft(self, arr: List[int], target: int) -> int:
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
+
+    def successfulPairs(
+        self, spells: List[int], potions: List[int], success: int
+    ) -> List[int]:
+        potions.sort()
+        m = len(potions)
+        max_potion = potions[m - 1]
+        answer = []
+        for spell in spells:
+            min_potion = -(success // -spell)
+            if min_potion > max_potion:
+                answer.append(0)
+                continue
+            index = self.bisectLeft(potions, min_potion)
+            answer.append(m - index)
+
+        return answer
