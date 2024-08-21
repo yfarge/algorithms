77c84bf951b652a029a2b65d5776484a12f79fef
Yousef Farge <yousef.farge@gmail.com>
2024-08-20 20:07:32 -0500

    k-closest-points-to-origin

---
+++ k-closest-points-to-origin/index.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda p: p[0] ** 2 + p[1] ** 2)
+        return points[:k]
