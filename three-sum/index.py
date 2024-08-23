4f8491b192aa3ec0bd97187ca0910dfb8a5b4e08
Yousef Farge <yousef.farge@gmail.com>
2024-08-22 23:04:46 -0500

    three-sum

--- three-sum/index.py
+++ three-sum/index.py
@@ -1,30 +1,29 @@
 class Solution:
     def threeSum(self, nums):
         nums.sort()
-        triplets = []
         n = len(nums)
+        triplets = []
+
         for i in range(n):
-            if i < n and i > 0 and nums[i - 1] == nums[i]:
+            if i > 0 and nums[i - 1] == nums[i]:
                 continue
+
             left, right = i + 1, n - 1
-            first_num = nums[i]
-            pair_target = 0 - first_num
+
             while left < right:
-                pair_sum = nums[left] + nums[right]
-                if pair_sum < pair_target:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
                     left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
                     while left < right and nums[left - 1] == nums[left]:
                         left += 1
-                elif pair_sum > pair_target:
-                    right -= 1
                     while left < right and nums[right + 1] == nums[right]:
                         right -= 1
-                else:
-                    triplets.append([first_num, nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while (left < right and nums[right + 1] == nums[right]
-                           and nums[left - 1] == nums[left]):
-                        left += 1
-                        right -= 1
+
         return triplets
+
