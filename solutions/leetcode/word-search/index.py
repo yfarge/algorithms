88c11fb87e0ef21018a670a5eb597d983218c87b
Yousef Farge <yousef.farge@gmail.com>
2024-09-27 17:40:43 -0500

    word-search

--- solutions/leetcode/word-search/index.py
+++ solutions/leetcode/word-search/index.py
@@ -1,45 +1,33 @@
-from typing import *
+from typing import List
 
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS = len(board), len(board[0])
-        path = set()
-        word_dict = Counter(word)
-        board_dict = Counter()
-
-        for i in range(ROWS):
-            for j in range(COLS):
-                board_dict[board[i][j]] += 1
-
-        if any(count > board_dict[char] for char, count in word_dict.items()):
-            return False
-
-        if board_dict[word[0]] > board_dict[word[-1]]:
-            word = word[::-1]
-
-        def dfs(r, c, i):
+        def backtrack(row: int, col: int, i: int):
             if i == len(word):
                 return True
-            if (r < 0 or c < 0 or
-                r >= ROWS or c >= COLS or
-                word[i] != board[r][c] or
-                    (r, c) in path):
+
+            if not (0 <= row < m and 0 <= col < n and board[row][col] == word[i]):
                 return False
 
-            path.add((r, c))
-
-            res = (dfs(r + 1, c, i + 1) or
-                   dfs(r - 1, c, i + 1) or
-                   dfs(r, c + 1, i + 1) or
-                   dfs(r, c - 1, i + 1))
-
-            path.remove((r, c))
-
-            return res
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (dfs(r, c, 0)):
+            board[row][col] = "#"
+            for next_row, next_col in [
+                (row - 1, col),
+                (row, col + 1),
+                (row + 1, col),
+                (row, col - 1),
+            ]:
+                if backtrack(next_row, next_col, i + 1):
                     return True
+
+            board[row][col] = word[i]
+            return False
+
+        m, n = len(board), len(board[0])
+        for row in range(m):
+            for col in range(n):
+                if backtrack(row, col, 0):
+                    return True
+
         return False
+
