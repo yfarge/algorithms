e4096e56b1650791fc441e5095824d4d02df4557
Yousef Farge <yousef.farge@gmail.com>
2024-09-29 23:48:43 -0500

    course-schedule (Topological Sort)

--- solutions/leetcode/course-schedule/index.py
+++ solutions/leetcode/course-schedule/index.py
@@ -2,6 +2,7 @@ from typing import List
 from collections import defaultdict
 
 
+# DFS
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         def detectCycle(course: int):
@@ -28,3 +29,33 @@ class Solution:
             if not visited[course] and detectCycle(course):
                 return False
         return True
+
+
+# Topological Sort (Kahn's Algorithm)
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i: [] for i in range(numCourses)}
+        for a, b in prerequisites:
+            graph[a].append(b)
+
+        indegree = [0] * numCourses
+        for i in range(numCourses):
+            for neighbor in graph[i]:
+                indegree[neighbor] += 1
+
+        queue = deque([])
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        visited = 0
+        while queue:
+            node = queue.popleft()
+            visited += 1
+
+            for neighbor in graph[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return visited == numCourses
