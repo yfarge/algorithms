38b5d6209e0fa883c7c23ca2621492adebe07a16
Yousef Farge <yousef.farge@gmail.com>
2024-10-02 02:42:20 -0500

    lru-cache (Custom Doubly Linked List)

--- solutions/leetcode/lru-cache/index.py
+++ solutions/leetcode/lru-cache/index.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 
 
+# OrderedDict
 class LRUCache:
     def __init__(self, capacity: int):
         self.cache = OrderedDict()
@@ -17,3 +18,58 @@ class LRUCache:
         self.cache.move_to_end(key)
         if len(self.cache) > self.capacity:
             self.cache.popitem(last=False)
+
+
+# Custom Doubly Linked List
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.entries = dict()
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.entries:
+            return -1
+
+        node = self.entries[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.entries:
+            node = self.entries[key]
+            node.val = value
+            self.remove(node)
+            self.add(node)
+        else:
+            node = ListNode(key, value)
+            self.add(node)
+            self.entries[key] = node
+
+        if len(self.entries) > self.capacity:
+            lru_node = self.head.next
+            self.remove(lru_node)
+            del self.entries[lru_node.key]
+
+    def add(self, node: ListNode):
+        previous_end = self.tail.prev
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node: ListNode):
+        node.next.prev = node.prev
+        node.prev.next = node.next
