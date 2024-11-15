1bfcc42f7d8a262fefc976f9286374fa20431c36
Yousef Farge <yousef.farge@gmail.com>
2024-11-14 21:02:43 -0600

    [leetcode]: valid-sudoku

--- solutions/leetcode/valid-sudoku/index.py
+++ solutions/leetcode/valid-sudoku/index.py
@@ -1,32 +1,26 @@
-class Solution(object):
-    def containsDuplicates(self, l):
-        l = list(filter(lambda x: x != '.', l))
-        return len(l) != len(set(l))
+from typing import List
+from collections import defaultdict
 
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        # Check row duplicates
-        for j in range(9):
-            if self.containsDuplicates(board[j]):
-                return False
 
-        # Check column duplicates
-        for j in range(9):
-            column = [row[j] for row in board]
-            if self.containsDuplicates(column):
-                return False
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        boxes = defaultdict(set)
 
-        # Check 3x3 sub-boxes
-        seen = set()
-        box_starts = [(i, j) for i in range(0, 9, 3) for j in range(0, 9, 3)]
-        for x_start, y_start in box_starts:
-            for column in range(9):
-                cell = board[x_start + column % 3][y_start + column // 3]
-                if cell != "." and cell in seen:
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    continue
+                if (
+                    board[row][col] in cols[col]
+                    or board[row][col] in rows[row]
+                    or board[row][col] in boxes[(row // 3, col // 3)]
+                ):
                     return False
-                seen.add(cell)
-            seen.clear()
+
+                cols[col].add(board[row][col])
+                rows[row].add(board[row][col])
+                boxes[(row // 3, col // 3)].add(board[row][col])
+
         return True
