8cee450a711cc715167ab9acd5a22e437bb6cee5
Yousef Farge <yousef.farge@gmail.com>
2025-02-12 21:17:55 -0600

    [leetcode]: minimum-number-of-operations-to-move-all-balls-to-each-box

--- solutions/leetcode/minimum-number-of-operations-to-move-all-balls-to-each-box/index.py
+++ solutions/leetcode/minimum-number-of-operations-to-move-all-balls-to-each-box/index.py
@@ -4,10 +4,24 @@ from typing import List
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        res = [0] * n
+        distances = [0] * n
+
+        prefixCount = 0
+        prefixSum = 0
 
         for i in range(n):
-            for j in range(n):
-                if boxes[j] == '1':
-                    res[i] += abs(i - j)
-        return res
+            distances[i] = prefixCount * i - prefixSum
+            if boxes[i] == '1':
+                prefixCount += 1
+                prefixSum += i
+
+        suffixCount = 0
+        suffixSum = 0
+
+        for i in range(n - 1, -1, -1):
+            distances[i] += suffixSum - suffixCount * i
+            if boxes[i] == '1':
+                suffixCount += 1
+                suffixSum += i
+
+        return distances
