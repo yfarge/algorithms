9db5254a8712f6abace3b965d981d4f599e852d4
Yousef Farge <yousef.farge@gmail.com>
2025-02-28 16:15:08 -0600

    [leetcode]: valid-anagram cpp

---
+++ solutions/leetcode/valid-anagram/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) {
+      return false;
+    }
+
+    int freq[26] = {0};
+    for (char c : s) {
+      freq[c - 'a']++;
+    }
+
+    for (char c : t) {
+      freq[c - 'a']--;
+
+      if (freq[c - 'a'] < 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
