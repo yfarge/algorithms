43fdec1d2e35546b191748da8774c758b0a23690
Yousef Farge <yousef.farge@gmail.com>
2025-03-05 19:51:19 -0600

    [leetcode]: search-in-rotated-sorted-array cpp

---
+++ solutions/leetcode/search-in-rotated-sorted-array/main.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (target == nums[mid]) {
+        return mid;
+      }
+
+      if (nums[left] <= nums[mid]) {
+        if (target > nums[mid] || target < nums[left]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      } else {
+        if (target < nums[mid] || target > nums[right]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
+};
