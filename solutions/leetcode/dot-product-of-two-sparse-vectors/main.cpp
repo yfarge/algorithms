a098cfde5264c97d3e4924b1ecb84f033a4f4c0f
Yousef Farge <yousef.farge@gmail.com>
2025-05-10 21:18:07 -0500

    [leetcode]: fix: dot-product-of-two-sparse-vectors

--- solutions/leetcode/dot-product-of-two-sparse-vectors/main.cpp
+++ solutions/leetcode/dot-product-of-two-sparse-vectors/main.cpp
@@ -1,29 +1,34 @@
-template <typename T>
-class SparseVector {
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+template <typename T> class SparseVector {
 private:
-    unordered_map<int, T> data;
+  unordered_map<int, T> data;
 
 public:
-    const T operator[](size_t index) const {
-        auto it = data.find(index);
-        return it != data.end() ? it->second : T{};
-    }
+  const T operator[](size_t index) const {
+    auto it = data.find(index);
+    return it != data.end() ? it->second : T{};
+  }
 
-    SparseVector(vector<T> &nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) continue;
-            data[i] = nums[i];
-        }
+  SparseVector(vector<T> &nums) {
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] == 0)
+        continue;
+      data[i] = nums[i];
     }
-    
-    // Return the dotProduct of two sparse vectors
-    int dotProduct(SparseVector& vec) {
-        int result = 0;
-        for (const auto &[key, value]: data) {
-            result += value * vec[key];
-        }
-        return result;
+  }
+
+  // Return the dotProduct of two sparse vectors
+  int dotProduct(SparseVector &vec) {
+    int result = 0;
+    for (const auto &[key, value] : data) {
+      result += value * vec[key];
     }
+    return result;
+  }
 };
 
 // Your SparseVector object will be instantiated and called as such:
