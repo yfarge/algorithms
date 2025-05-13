fc726622f9eb08109d36a6b367eaa8584228fbc8
Yousef Farge <yousef.farge@gmail.com>
2025-05-13 15:23:24 -0500

    [leetcode]: partition-array-according-to-given-pivot

---
+++ solutions/leetcode/partition-array-according-to-given-pivot/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> pivotArray(vector<int> &nums, int pivot) {
+    vector<int> result(nums.size());
+    int left = 0;
+    int right = nums.size() - 1;
+
+    for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
+      if (nums[i] < pivot) {
+        result[left] = nums[i];
+        left++;
+      }
+
+      if (nums[j] > pivot) {
+        result[right] = nums[j];
+        right--;
+      }
+    }
+    while (left <= right) {
+      result[left] = pivot;
+      left++;
+    }
+
+    return result;
+  }
+};
