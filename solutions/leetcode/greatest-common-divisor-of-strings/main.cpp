6a1894c90801e5069a06b5cafb41306e7cad1bab
Yousef Farge <yousef.farge@gmail.com>
2025-05-24 21:47:16 -0500

    [leetcode]: greatest-common-divisor-of-strings

---
+++ solutions/leetcode/greatest-common-divisor-of-strings/main.cpp
@@ -0,0 +1,15 @@
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        return str1.substr(0, gcd(str1.size(), str2.size()));
+    }
+};
